d7f51f4b7751b2dd3ca9a2eb59a599350f5100ef
Natalia Pechatnova <pechatnova.n@dterra.ru>
2025-03-15 20:12:55 +0300

    update 404 page

--- src/components/pages/NotFoundPage.vue
+++ src/components/pages/NotFoundPage.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <h1>404 - Такой страницы не существует</h1>
+  <div class="not-found">
+    <h1 class="not-found__title">404</h1>
+    <p class="not-found__text">Такой страницы не существует</p>
   </div>
 </template>
 
@@ -10,5 +11,17 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-  
+  .not-found {
+    padding: 60px 0 120px;
+    &__title {
+      text-align: center;
+      font-size: 48px;
+      font-weight: 600;
+      margin-bottom: 20px;
+    }
+    &__text {
+      text-align: center;
+      font-size: 32px;
+    }
+  }
 </style>
